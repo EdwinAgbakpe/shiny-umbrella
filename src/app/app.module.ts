0b838b432e0067ae8eeac9de98193b75591259b3
EdwinAgbakpe <kagbakpe@gmail.com>
2021-11-12 07:40:41 +0000

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginCardComponent } from './login-card/login-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
