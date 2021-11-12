c8baaccb2260aeb480f4fe3e6e35ca535eb197c6
EdwinAgbakpe <kagbakpe@gmail.com>
2021-11-12 11:43:29 +0000

    test/rm 1 test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginCardComponent } from './login-card/login-card.component';
+import { BgAnimationComponent } from './bg-animation/bg-animation.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginCardComponent,
+        BgAnimationComponent,
       ],
     }).compileComponents();
   });
@@ -26,10 +32,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('shiny-umbrella');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shiny-umbrella app is running!');
-  });
 });
